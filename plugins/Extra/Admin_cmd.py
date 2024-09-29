8d14e8c6b908ace48199c4cc4514f7e4c9330f8a
TMZBotz <167861324+TMZBotz@users.noreply.github.com>
2024-09-29 18:34:15 +0530

    Update Admin_cmd.py

--- plugins/Extra/Admin_cmd.py
+++ plugins/Extra/Admin_cmd.py
@@ -15,7 +15,7 @@ async def admin_cmd(client, message):
         [KeyboardButton("/deleteall"), KeyboardButton("/delete")],
         [KeyboardButton("/Commands"), KeyboardButton("/restart")],
         [KeyboardButton("All These Commands Can Be Used Only By Admins.")],
-        [KeyboardButton("⚡ powered by @JISSHU_BOTS")]
+        [KeyboardButton("⚡ Powered By Kᴜᴛᴛʏ 💖")]
     ]
     reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True, one_time_keyboard=True)
     
